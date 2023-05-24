a15ccc77f35874bdcca728fc721768d0ab73b45c
Devendra Parkar <devendra.parkar@asu.edu>
2023-05-24 13:12:21 -0700

    Added 6 test-cases to test the shooting implementation

---
+++ tests/follow_bullet/follow_bullet_test.py
@@ -0,0 +1,24 @@
+import pyreason_gym
+import gym
+import time
+
+env = gym.make('PyReasonGridWorld-v0', render_mode='human')
+obs = env.reset()
+
+# Sample actions:
+action = {
+    'red_team': [5],
+    'blue_team': [1]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+for _ in range(10):
+    action = {
+        'red_team': [1],
+        'blue_team': [1]
+    }
+    obs = env.step(action)
+    print(obs)
+    time.sleep(1)
+env.close()
