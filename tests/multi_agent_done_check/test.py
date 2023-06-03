4fefcf3de7e12860caff1d5bb7732671112b00ff
Devendra Parkar <devendra.parkar@asu.edu>
2023-06-03 13:45:33 -0700

    Added multi-agent testcases

---
+++ tests/multi_agent_done_check/test.py
@@ -0,0 +1,57 @@
+import pyreason_gym
+import gym
+import time
+
+env = gym.make('PyReasonGridWorld-v0', render_mode='human', num_agents_per_team=2)
+obs = env.reset()
+
+action = {
+    'red_team': [7,7],
+    'blue_team': [1,3]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
+    'red_team': [1,2],
+    'blue_team': [1,2]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
+    'red_team': [1,2],
+    'blue_team': [1,3]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
+    'red_team': [1,2],
+    'blue_team': [1,2]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
+    'red_team': [2,3],
+    'blue_team': [0,1]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
+    'red_team': [2,3],
+    'blue_team': [0,3]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
+    'red_team': [2,3],
+    'blue_team': [0,3]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+env.close()
