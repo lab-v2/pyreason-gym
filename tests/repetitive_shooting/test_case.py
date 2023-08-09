17f32ca4a1ab11c64af72ea31798a56ed808b666
Devendra Parkar <devendra.parkar@asu.edu>
2023-08-08 22:48:46 -0700

    changed repeat shoot test

--- tests/repetitive_shooting/test_case.py
+++ tests/repetitive_shooting/test_case.py
@@ -14,15 +14,22 @@ obs = env.step(action)
 print(obs)
 time.sleep(1)
 action = {
+    'red_team': [1],
+    'blue_team': [1]
+}
+obs = env.step(action)
+print(obs)
+time.sleep(1)
+action = {
     'red_team': [7],
     'blue_team': [1]
 }
 obs = env.step(action)
 print(obs)
 time.sleep(1)
-for _ in range(10):
+for _ in range(5):
     action = {
-        'red_team': [2],
+        'red_team': [1],
         'blue_team': [1]
     }
     obs = env.step(action)
