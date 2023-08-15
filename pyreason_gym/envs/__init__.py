7678509850ed4c17e2a91a9e5394dc525413639f
Dyuman <dyuman2001@gmail.com>
2023-08-15 18:26:24 +0530

    updated test and fixed bug

--- pyreason_gym/envs/__init__.py
+++ pyreason_gym/envs/__init__.py
@@ -1,2 +1,1 @@
-from pyreason_gym.envs.grid_world import GridWorldEnv
 from pyreason_gym.envs.map_world import MapWorldEnv
--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import pyreason_gym
 import gym
+import time
 
 env = gym.make('PyReasonMapWorld-v0', start_point='202811335', end_point='472254560', graph_path='./pyreason_gym/pyreason_map_world/graph/map_graph_sub.graphml', rules_path='pyreason_gym/pyreason_map_world/rules/rules.txt')
 obs = env.reset()
 print(obs)
 
 # Randomly sample actions from the action space
-for i in range(10):
+for i in range(20):
     action = env.action_space.sample()
-    print('action',action)
+    print('action', action)
+    s = time.time()
     obs, rew, done, truncated, info = env.step(action)
+    e = time.time()
+    print('TIME,', e-s)
     print(obs)
     print(rew)
     print()
