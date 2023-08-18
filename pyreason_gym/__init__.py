262824923897847c87e2ad1c9d1b27b48aca4425
Dyuman Aditya <dyuman2001@gmail.com>
2023-08-18 12:11:30 +0530

    Update __init__.py

--- pyreason_gym/__init__.py
+++ pyreason_gym/__init__.py
@@ -1,9 +1,5 @@
 from gym.envs.registration import register
 
-register(
-    id='PyReasonGridWorld-v0',
-    entry_point='pyreason_gym.envs:GridWorldEnv'
-)
 
 register(
     id='PyReasonMapWorld-v0',
